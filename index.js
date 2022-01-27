5e9b94a7cac2304cd073e16982658e668002bfc3
R3tr8 <39610870+R3tr8@users.noreply.github.com>
2022-01-27 01:02:31 -0400

    ajout de qoa + recherche avancée

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 const puppeteer = require('puppeteer');
-const mouth = '03';
-const year = 2022;
+const qoa = require('qoa');
+const {log} = console;
+const input = {
+    type: 'input',
+    query: 'Entrer le titre du film que vous voulez rechercher',
+    handle: 'movie'
+}
+qoa.prompt([input]).then(searchMovie);
 
-(async () => {
-    const browser = await puppeteer.launch({headless: true});
-    const page = await browser.newPage();
-    await page.goto(`https://www.imdb.com/movies-coming-soon/${year}-${mouth}/`);
-    const movies = await page.evaluate(() => {
-        let elements = document.querySelectorAll('div.list_item');
-        let movies = [];
-        for (let element of elements) {
-            movies.push({
-                title: element.querySelector('td.overview-top a').text.trim(),
-                img: element.querySelector('img').src,
-                director:element.querySelector('div.txt-block a').text.trim(),
-                description: element.querySelector('div.outline').textContent.trim(),
-            });
+function searchMovie(movie) {
+    puppeteer.launch({headless: false}).then(async browser => {
+        const page = await browser.newPage();
+        await page.goto(`https://www.imdb.com/`, {waitUntil: 'networkidle2'});
+
+        const searchInput = await page.$('#suggestion-search');
+        await searchInput.click();
+        await searchInput.type(JSON.stringify(movie.movie));
+
+        await page.waitForTimeout(5000);
+
+        const searchButton = await page.$('#iconContext-magnify');
+        await searchButton.click();
+
+        console.log('searching...');
+
+        await page.waitForTimeout(5000);
+
+        let elements = await page.$$('table.findList tr');
+        let movies = []
+        for (let i = 0; i <= movies.length; i++) {
+            let element = elements[i];
+            let title = await element.$eval('a', el => el.innerText);
+            console.log(title);
+            movies.push(elements[i]);
+            //TODO : add titles to array
         }
-        return movies;
+        //TODO : display movies
+
+        await page.waitForTimeout(5000);
     });
-    console.log(movies);
-    await browser.close();
-})();
+}
